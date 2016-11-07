d810855922f4a7e6a23733d26124e73e86d62f91
yaoshuhong <yaoshuhong@shengzhidai.com>
2016-11-07 16:42:22 +0800

    微信统一下单更新入参, 同时暂时配置deploy的地址为f5zone私服

--- wechat/src/main/java/org/treeleaf/thirdchannel/wechat/pay/entity/UnifiedOrder.java
+++ wechat/src/main/java/org/treeleaf/thirdchannel/wechat/pay/entity/UnifiedOrder.java
@@ -32,9 +32,24 @@ public class UnifiedOrder {
     private String sign;
 
     /**
+     * 签名类型,目前支持HMAC-SHA256和MD5，默认为MD5
+     */
+    private String sign_type;
+
+    /**
      * 商品或支付单简要描述
      */
     private String body;
+
+    /**
+     * 商品详细列表，使用Json格式，传输签名前请务必使用CDATA标签将JSON文本串保护起来。
+     */
+    private String detail;
+
+    /**
+     * 附加数据,附加数据，在查询API和支付通知中原样返回，该字段主要用于商户携带订单的自定义数据
+     */
+    private String attach;
 
     /**
      * 商户系统内部的订单号,32个字符内、可包含字母
@@ -57,6 +72,21 @@ public class UnifiedOrder {
     private String spbill_create_ip;
 
     /**
+     * 交易起始时间,订单生成时间，格式为yyyyMMddHHmmss，如2009年12月25日9点10分10秒表示为20091225091010
+     */
+    private String time_start;
+
+    /**
+     * 交易结束时间,订单失效时间，格式为yyyyMMddHHmmss，如2009年12月27日9点10分10秒表示为20091227091010
+     */
+    private String time_expire;
+
+    /**
+     * 商品标记,商品标记，代金券或立减优惠功能的参数，说明详见代金券或立减优惠
+     */
+    private String goods_tag;
+
+    /**
      * 接收微信支付异步通知回调地址
      */
     private String notify_url;
@@ -65,6 +95,16 @@ public class UnifiedOrder {
      * 取值如下：JSAPI，NATIVE，APP，WAP
      */
     private String trade_type;
+
+    /**
+     * 商品ID,trade_type=NATIVE，此参数必传。此id为二维码中包含的商品ID，商户自行定义。
+     */
+    private String product_id;
+
+    /**
+     * 指定支付方式, no_credit--指定不能使用信用卡支付
+     */
+    private String limit_pay;
 
     /**
      * 用户标识,trade_type=JSAPI，此参数必传
@@ -173,5 +213,69 @@ public class UnifiedOrder {
 
     public void setOpenid(String openid) {
         this.openid = openid;
+    }
+
+    public String getSign_type() {
+        return sign_type;
+    }
+
+    public void setSign_type(String sign_type) {
+        this.sign_type = sign_type;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getAttach() {
+        return attach;
+    }
+
+    public void setAttach(String attach) {
+        this.attach = attach;
+    }
+
+    public String getTime_start() {
+        return time_start;
+    }
+
+    public void setTime_start(String time_start) {
+        this.time_start = time_start;
+    }
+
+    public String getTime_expire() {
+        return time_expire;
+    }
+
+    public void setTime_expire(String time_expire) {
+        this.time_expire = time_expire;
+    }
+
+    public String getGoods_tag() {
+        return goods_tag;
+    }
+
+    public void setGoods_tag(String goods_tag) {
+        this.goods_tag = goods_tag;
+    }
+
+    public String getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(String product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getLimit_pay() {
+        return limit_pay;
+    }
+
+    public void setLimit_pay(String limit_pay) {
+        this.limit_pay = limit_pay;
     }
 }
